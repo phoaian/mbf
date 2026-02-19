24378831f2491b8d4e3ec6768d89a5843c596f08
phoaian <nguyenatp@gmail.com>
2026-02-19 21:45:59 +0700

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,4 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth"; // Đảm bảo đường dẫn này trỏ đúng tới file cấu hình authOptions của bạn
-
+import { authOptions } from "@/lib/auth";
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
